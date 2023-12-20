9f74880d276aaf3e8a77e9e31b8298fbdf699108
Rodrigo Patino <developer.rodrigo.patino@gmail.com>
2023-12-20 01:57:40 -0600

    Validate

--- internal/reader/infrastructure/reader.go
+++ internal/reader/infrastructure/reader.go
@@ -1,7 +1,6 @@
 package infrastructure
 
 import (
-	"log"
 	"stori/pkg/transaction"
 
 	"github.com/jszwec/csvutil"
@@ -10,15 +9,12 @@ import (
 type ReaderCSV struct{}
 
 func (r *ReaderCSV) Read(data []byte) ([]transaction.Transaction, error) {
-	log.Println("read method from ReaderCSV")
-
 	var transactions transaction.Collection
-	err := csvutil.Unmarshal(data, &transactions)
-	if err != nil {
+	if err := csvutil.Unmarshal(data, &transactions); err != nil {
 		return nil, err
 	}
 
-	if err = transactions.Validate(); err != nil {
+	if err := transactions.Validate(); err != nil {
 		return nil, err
 	}
 
