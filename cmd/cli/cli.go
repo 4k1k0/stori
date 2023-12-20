c5256652c549fefcf1492c3e3250d3298d09da48
Rodrigo Patino <developer.rodrigo.patino@gmail.com>
2023-12-19 21:52:31 -0600

    WIP

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"log"
 	"stori/pkg/processor/application"
-	"stori/pkg/transaction"
 )
 
 func main() {
@@ -18,34 +17,6 @@ func main() {
 	}
 
 	log.Println(res)
-
-	checkData()
-
-}
-
-func checkData() {
-	foo("")
-	foo("9")
-	foo("1")
-	foo("12.2")
-	foo("+")
-	foo("+1")
-	foo("+1.")
-	foo("+1.2")
-	foo("-1.2")
-	foo("-1.")
-	foo("-1")
-	foo("-")
-	foo("-1.2s")
-	foo("as-1.2")
-}
-
-func foo(d string) {
-	trx := transaction.Transaction{Date: "1/11", Transaction: d}
-	err := trx.Validate()
-	if err != nil {
-		log.Println(err)
-	}
 }
 
 func getFlags() string {
