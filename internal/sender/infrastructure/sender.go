19d1a6e90fa44fcfd9188858c34943ae2e6eb5a0
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2023-12-22 17:14:29 -0600

    Enable sender

--- internal/sender/infrastructure/sender.go
+++ internal/sender/infrastructure/sender.go
@@ -2,6 +2,7 @@ package infrastructure
 
 import (
 	"log"
+	"stori/internal/config"
 	"stori/pkg/result"
 )
 
@@ -11,7 +12,7 @@ type EmailSender struct {
 
 func (e *EmailSender) Send(r result.Result, data []byte) error {
 	log.Println("Send email from EmailSender")
-	// html := CreateTemplate(r)
-	// sendEmail(html, config.Config().Email)
+	html := CreateTemplate(r)
+	sendEmail(html, config.Config().Email)
 	return nil
 }
