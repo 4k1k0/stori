b32c7f5ac08548254b151bd0f0e63aa1cae9dcd9
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2023-12-22 15:49:55 -0600

    Lambda test

--- cmd/lambda/lambda.go
+++ cmd/lambda/lambda.go
@@ -3,20 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Filename string `json:"filename,omitempty"`
-	Email    string `json:"email,omitempty"`
-}
-
-func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
-	if event == nil {
-		return nil, fmt.Errorf("received nil event")
-	}
-	message := fmt.Sprintf("Filename %q! Email %q", event.Filename, event.Email)
+func HandleRequest(ctx context.Context, event events.S3EventRecord) (*string, error) {
+	message := fmt.Sprintf("Event: %+v", event)
+	log.Println(message)
 	return &message, nil
 }
 
